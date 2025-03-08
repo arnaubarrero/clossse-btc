e71a2a76b74b4f814d45ee74f2a59a80c3b50499
arnaubarrero <arnau.baso@gmail.com>
2025-03-08 10:29:31 +0100

    Modo oscuro del search

--- next-frontend-clossse/src/app/user/search/page.jsx
+++ next-frontend-clossse/src/app/user/search/page.jsx
@@ -15,19 +15,20 @@ export default function Home() {
     }, [router]);
 
     return (
-        <div className="min-h-screen bg-black text-white">
+        <div className="min-h-screen bg-white text-black dark:bg-black dark:text-white transition-colors duration-300">
             <div className="container mx-auto p-4">
                 <h1 className="text-3xl font-bold text-blue-500 mb-8 text-center">
-                    Search a User
+                    CLOSSSE
                 </h1>
 
-
                 <div className="flex items-center justify-center space-x-4 mb-8">
-                    <input type="text" placeholder="Search User"
-                        className="w-full transition-[1s] max-w-md px-4 py-2 bg-gray-800 text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    <input
+                        type="text"
+                        placeholder="Search User"
+                        className="w-full max-w-md px-4 py-2 bg-gray-100 text-black rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-800 dark:text-white transition-colors duration-300"
                     />
                     <button className="p-2 bg-blue-500 rounded-lg hover:bg-blue-600 transition duration-300">
-                        <Image src="/qr.svg" width={25} height={25} alt="QR" />
+                        <Image src="/qr.svg" width={25} height={25} alt="QR" className="filter  dark:filter-none" />
                     </button>
                 </div>
             </div>
