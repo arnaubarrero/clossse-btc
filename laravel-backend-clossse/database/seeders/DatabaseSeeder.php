b61c7f7fde9bc77f348ddf4af1417c7784c77bfe
arnaubarrero <arnau.baso@gmail.com>
2025-03-13 19:14:12 +0100

    Sider usuario

--- laravel-backend-clossse/database/seeders/DatabaseSeeder.php
+++ laravel-backend-clossse/database/seeders/DatabaseSeeder.php
@@ -1,23 +1,32 @@
 <?php
+    namespace Database\Seeders;
 
-namespace Database\Seeders;
+    use App\Models\User;
+    use App\Models\Wallet;
+    use Illuminate\Database\Seeder;
+    use Illuminate\Support\Facades\Hash;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
+    class DatabaseSeeder extends Seeder {
+        public function run(): void {
+            $users = [
+                [
+                    'name' => 'Arnau', 
+                    'apellidos' => 'Barrero', 
+                    'email' => 'arnau.baso@gmail.com', 
+                    'username' => 'arni_baso', 
+                    'password' => Hash::make('Arnau_2004')
+                ],
+            ];
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
+            foreach ($users as $userData) {
+                $user = User::create($userData);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+                Wallet::create([
+                    'user_id' => $user->id,
+                    'public_address' => '1KXGrLaKRgaHgFmzJ9LvrSXduCWXr2Kqcg',
+                    'private_key' => 'KyQcerULBQQCbVQHrrvFhAm49L9LSPKNFBForysMcAkdSiSC7fX4',
+                    'wallet' => 'wallet_67d1d4c1f3a6d'
+                ]);
+            }
+        }
     }
-}
